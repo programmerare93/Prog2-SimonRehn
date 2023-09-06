14db484fbacb6b9bb7a9888e8e79753725d3210e
Simon Rehn <quesadilla.guerilla@protonmail.com>
2023-09-06 10:47:54 +0200

    löst happyprime

--- kattis/happyprime.cpp
+++ kattis/happyprime.cpp
@@ -1,6 +1,12 @@
 #include <array>
 #include <string>
+#include <vector>
 #include <iostream>
+
+int char_to_int(char c)
+{
+        return c - '0';
+}
 
 bool is_prime(int num)
 {
@@ -15,6 +21,45 @@ bool is_prime(int num)
         }
 
         return true;
+}
+
+void square_ints(std::vector<int>& vec)
+{
+        for (int& num : vec) {
+                num *= num;
+        }
+}
+
+int ints_sum(std::vector<int> const& vec)
+{
+        int sum = 0;
+        for (int num : vec) {
+                sum += num;
+        }
+
+        return sum;
+}
+
+bool is_happy(int num)
+{
+        std::string num_str = std::to_string(num);
+
+        std::vector<int> digits;
+        for (char c : num_str) {
+                digits.push_back(char_to_int(c));
+        }
+
+        square_ints(digits);
+        /* Ifall summan blir 'num' igen kommer talet inte vara happy */
+        while (ints_sum(digits) != 1 && ints_sum(digits) != num) {
+                square_ints(digits);
+        }
+
+        if (ints_sum(digits) == 1) {
+                return true;
+        } else {
+                return false;
+        }
 }
 
 std::array<int, 2> parse_input(std::string const& input)
@@ -36,6 +81,9 @@ std::array<int, 2> parse_input(std::string const& input)
                 }
         }
 
+        /* Ser till så att sista nummeret i inputen läggs til*/
+        arr[index] = std::stoi(num);
+
         return arr;
 
 }
@@ -46,9 +94,13 @@ void handle_input(std::array<int, 2> const& input)
         int m = input[1];
 
         if (is_prime(m)) {
-                std::cout << "Prime\n";
+                if (is_happy(m)) {
+                        std::cout << K << ' ' << m << " YES\n";
+                } else {
+                        std::cout << K << ' ' << m << " NO\n";
+                }
         } else {
-                std::cout << "Not Prime\n";
+                std::cout << K << ' ' << m << " NO\n";
         }
 }
 
